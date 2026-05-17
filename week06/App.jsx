f8d916603c0120add7b5140f80eef66de476557f
imly-dia <imlyx.xdia@gmail.com>
2026-05-17 18:18:39 +0900

    Add files via upload

---
+++ week06/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import SignupCard from './components/SignupCard';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <SignupCard />
+    </div>
+  );
+}
+
+export default App;
